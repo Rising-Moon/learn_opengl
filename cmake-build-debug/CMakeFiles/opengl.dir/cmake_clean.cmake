badb95abe9238d79aeddb6166dec568280563aee
Rising-Moon <767644624@qq.com>
2020-12-11 00:58:57 +0800

    初始化仓库

---
+++ cmake-build-debug/CMakeFiles/opengl.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opengl.dir/lib/glad-c/src/glad.c.obj"
+  "CMakeFiles/opengl.dir/main.cpp.obj"
+  "opengl.exe"
+  "opengl.exe.manifest"
+  "opengl.lib"
+  "opengl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/opengl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/lib/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "F:/MyProject/CProject/learn_opengl/lib/glfw/src/context.c" "F:/MyProject/CProject/learn_opengl/cmake-build-debug/lib/glfw/src/CMakeFiles/glfw.dir/context.c.obj"
+  "F:/MyProject/CProject/learn_opengl/lib/glfw/src/egl_context.c" "F:/MyProject/CProject/learn_opengl/cmake-build-debug/lib/glfw/src/CMakeFiles/glfw.dir/egl_context.c.obj"
+  "F:/MyProject/CProject/learn_opengl/lib/glfw/src/init.c" "F:/MyProject/CProject/learn_opengl/cmake-build-debug/lib/glfw/src/CMakeFiles/glfw.dir/init.c.obj"
+  "F:/MyProject/CProject/learn_opengl/lib/glfw/src/input.c" "F:/MyProject/CProject/learn_opengl/cmake-build-debug/lib/glfw/src/CMakeFiles/glfw.dir/input.c.obj"
+  "F:/MyProject/CProject/learn_opengl/lib/glfw/src/monitor.c" "F:/MyProject/CProject/learn_opengl/cmake-build-debug/lib/glfw/src/CMakeFiles/glfw.dir/monitor.c.obj"
+  "F:/MyProject/CProject/learn_opengl/lib/glfw/src/osmesa_context.c" "F:/MyProject/CProject/learn_opengl/cmake-build-debug/lib/glfw/src/CMakeFiles/glfw.dir/osmesa_context.c.obj"
+  "F:/MyProject/CProject/learn_opengl/lib/glfw/src/vulkan.c" "F:/MyProject/CProject/learn_opengl/cmake-build-debug/lib/glfw/src/CMakeFiles/glfw.dir/vulkan.c.obj"
+  "F:/MyProject/CProject/learn_opengl/lib/glfw/src/wgl_context.c" "F:/MyProject/CProject/learn_opengl/cmake-build-debug/lib/glfw/src/CMakeFiles/glfw.dir/wgl_context.c.obj"
+  "F:/MyProject/CProject/learn_opengl/lib/glfw/src/win32_init.c" "F:/MyProject/CProject/learn_opengl/cmake-build-debug/lib/glfw/src/CMakeFiles/glfw.dir/win32_init.c.obj"
+  "F:/MyProject/CProject/learn_opengl/lib/glfw/src/win32_joystick.c" "F:/MyProject/CProject/learn_opengl/cmake-build-debug/lib/glfw/src/CMakeFiles/glfw.dir/win32_joystick.c.obj"
+  "F:/MyProject/CProject/learn_opengl/lib/glfw/src/win32_monitor.c" "F:/MyProject/CProject/learn_opengl/cmake-build-debug/lib/glfw/src/CMakeFiles/glfw.dir/win32_monitor.c.obj"
+  "F:/MyProject/CProject/learn_opengl/lib/glfw/src/win32_thread.c" "F:/MyProject/CProject/learn_opengl/cmake-build-debug/lib/glfw/src/CMakeFiles/glfw.dir/win32_thread.c.obj"
+  "F:/MyProject/CProject/learn_opengl/lib/glfw/src/win32_time.c" "F:/MyProject/CProject/learn_opengl/cmake-build-debug/lib/glfw/src/CMakeFiles/glfw.dir/win32_time.c.obj"
+  "F:/MyProject/CProject/learn_opengl/lib/glfw/src/win32_window.c" "F:/MyProject/CProject/learn_opengl/cmake-build-debug/lib/glfw/src/CMakeFiles/glfw.dir/win32_window.c.obj"
+  "F:/MyProject/CProject/learn_opengl/lib/glfw/src/window.c" "F:/MyProject/CProject/learn_opengl/cmake-build-debug/lib/glfw/src/CMakeFiles/glfw.dir/window.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "MSVC")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "_CRT_SECURE_NO_WARNINGS"
+  "_GLFW_USE_CONFIG_H"
+  "_UNICODE"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../lib/glfw/include"
+  "../lib/glfw/src"
+  "lib/glfw/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
